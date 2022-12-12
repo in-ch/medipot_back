fb2564c0f6c0939e94c4b326c55584f90eb050d4
in-ch <sxin2949@naver.com>
2022-12-12 23:16:03 +0900

    docs(location): location service에 주석 추가

--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -12,6 +12,12 @@ export class LocationService {
         @InjectRepository(Location) private readonly locations: Repository<Location>,
     ) {}
 
+    /**
+     * @param {PaginationDto} query 쿼리값
+     * @description 입지 정보들을 가져온다.
+     * @return {OutputDto<LocationOutputCrudDto[]>} 입지 정보를 가져온다.
+     * @author in-ch, 2022-12-07
+    */
     async getLocations(query: PaginationDto): Promise<OutputDto<LocationOutputCrudDto[]>> {
         const {limit, page} = query;
 
@@ -33,6 +39,13 @@ export class LocationService {
             }
         }
     }    
+
+    /**
+     * @param {LocationCrudDto} payload 생성할 입지 정보들
+     * @description 입지 정보들을 가져온다.
+     * @return {OutputDto<LocationOutputCrudDto>} 입지 생성 후 결과를 알려준다.
+     * @author in-ch, 2022-12-07
+    */
     async createLocation(payload: LocationCrudDto): Promise<OutputDto<LocationOutputCrudDto>> {
         try{
             const newLocation = this.locations.create(payload);

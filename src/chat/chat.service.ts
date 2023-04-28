475e424b8ef48e694109e4629d5a2f24b9dd9130
in-ch <sxin2949@naver.com>
2023-04-28 19:54:06 +0900

    fix(chat): Change chat list ORDER createdAt DESC -> ASC

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -73,7 +73,7 @@ export class ChatService {
         relations: ['toUser', 'fromUser'],
         loadRelationIds: true,
         order: {
-          createdAt: 'DESC',
+          createdAt: 'ASC',
         },
       });
       return {

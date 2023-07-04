5a0cf33e0744cb0f2149561d4d9e22465ca65f33
in-ch <sxin2949@naver.com>
2023-07-04 21:53:51 +0900

    fix: env connection error

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -6,8 +6,7 @@ import * as Joi from 'joi';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'dev' ? '.dev.env' : '.prod.env',
-      ignoreEnvFile: process.env.NODE_ENV === '.prod.env',
+      envFilePath: `.${process.env.NODE_ENV}.env`,
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('dev', 'prod').required(),
         DB_HOST: Joi.string().required(),

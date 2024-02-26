d13dcfab5a5f700034fb3cbcd76345a4244ac5ce
in-ch <sxin2949@naver.com>
2024-02-26 21:49:36 +0900

    feat(uploads): 이미지 업로드에 워터마크 기능 추가 (#31)

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -4,9 +4,14 @@ import { ApiTags } from '@nestjs/swagger';
 import * as AWS from 'aws-sdk';
 
 const sharp = require('sharp');
-const BUCKET_NAME = 'medipot-uploads';
 
-const generateRandomString = (num) => {
+/**
+ * @param num 문자열 길이
+ * @description 인자로받은 문자열 길이만큼의 랜덤 문자열을 생성합니다.
+ *              이때 날짜값도 앞에 추가합니다.
+ * @returns {string} 랜덤 문자열
+ */
+const generateRandomString = (num: number): string => {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
   let result = '';
   const charactersLength = characters.length;
@@ -36,25 +41,35 @@ export class UploadsController {
         secretAccessKey: process.env.S3_SECRET_KEY,
       },
     });
-    const Image = await sharp(file.buffer, { failOnError: false })
-      .withMetadata()
-      .resize(1000)
-      .jpeg({ mozjpeg: true })
-      .png()
-      .toBuffer();
 
     try {
+      const s3 = new AWS.S3();
+      const imgSize = 700;
+
+      // 이미지 리사이징 및 워터마크 적용
+      const Image = await sharp(file.buffer, { failOnError: false })
+        .withMetadata()
+        .resize(imgSize)
+        .jpeg({ mozjpeg: true })
+        .png()
+        .toBuffer();
+
+      const watermarkedImage = await sharp(Image)
+        .composite([{ input: process.env.WATERMARK_IMG, gravity: 'southeast' }])
+        .resize({ width: imgSize })
+        .toBuffer();
+
       const objectName = `${generateRandomString(10)}.png`;
-      await new AWS.S3()
+      await s3
         .putObject({
-          Body: Image,
-          Bucket: BUCKET_NAME,
+          Body: watermarkedImage,
+          Bucket: process.env.S3_BUCKET_NAME,
           Key: objectName,
-          ContentEncoding: 'base64',
           ContentType: 'image/png',
           ACL: 'public-read',
         })
         .promise();
+
       const url = `${process.env.CLOUD_FRONT}/${objectName}`;
       return { url };
     } catch (e) {
